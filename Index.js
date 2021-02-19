4d025f0132fb68b005e7a62203f206426df8c58e
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-02-18 16:45:41 -0800

    initial questions array created.  still need to create array of questions for adding other team members

--- Index.js
+++ Index.js
@@ -6,3 +6,36 @@ const Engineer = requier('./lib/Engineer');
 
 // this module will run the inquirer prompts and then generate the template based on the users input.
 
+// inquirer questions:
+const questions = [
+    {
+        // team manager question: name
+        type: "input",
+        name: "name",
+        message: "What is the Team Manager's name?  "
+    },
+    {
+        // team manager question: id
+        type: "input",
+        name: "id",
+        message: "Please enter the Team Manager's ID:  "
+    },
+    {
+        // team manager question: email
+        type: "input",
+        name: "email",
+        message: "Please enter the Team Manager's email address:  "
+    },
+    {
+        // team manager question: office number
+        type: "input",
+        name: "officeNumber",
+        message: "Please enter the Team Manager's office number:  "
+    },
+    {
+        // additional team members?
+        type: "confirm",
+        name: "otherMembers",
+        message: "Do you have additional team members to add to your team roster?  "
+    }
+];
