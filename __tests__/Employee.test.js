bf082dcc1dca8e29d04dfd9c899e0e420360bbc6
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-02-20 09:42:43 -0800

    tests for employee pass

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,25 +1,22 @@
 const { TestScheduler } = require('jest');
 const Employee = require('../lib/Employee');
 
-
-// test for employee name
-test ('checks to see if employee has a name of Nate', ()=> {
-    const employee = new Employee('Nate', '564', 'nate@nate.com')
+// test for employee name, id, and email
+test ('checks to see if employee name is string', ()=> {
+    const employee = new Employee('Nate', '564', 'nate@nate.com');
     
     expect(employee.name).toBe('Nate');
-});
-
-// test for employee id
-test ('checks to see if employee has an ID of 564', ()=> {
-    const employee = new Employee('Nate', '564', 'nate@nate.com')
-    
     expect(employee.id).toBe('564');
+    expect(employee.email).toBe('nate@nate.com');
 });
 
+// test for employee name, id, and email
+test ('checks to see if employee name is string', ()=> {
+    const employee = new Employee('Nate', '564', 'nate@nate.com');
+    
+    expect(employee.getName()).toBe('Nate');
+    expect(employee.getId()).toBe('564');
+    expect(employee.getEmail()).toBe('nate@nate.com');
 
-test('checks to ')
-
-
-
-
-// create tests to verify that the employee constructor and its methods are working.
+    expect(employee.getRole()).toBe('Employee');
+});
