f24fb785cd6772f31c94bb212dcced6490fd61fb
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-02-20 10:17:46 -0800

    tests pass! could do different tests but I think this is good enough to turn in

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,24 @@
 const { TestScheduler } = require('jest');
+const Intern = require('../lib/Intern');
+
+// test for Intern name, id, and email
+test ('checks to see if Intern name is string', ()=> {
+    const intern = new Intern('Nate', '564', 'nate@nate.com', 'UCLA');
+    
+    expect(intern.name).toBe('Nate');
+    expect(intern.id).toBe('564');
+    expect(intern.email).toBe('nate@nate.com');
+});
+
+// test for Intern getname, getid,  getemail, and getgithub methods 
+test ('checks to see if Intern name is string', ()=> {
+    const intern = new Intern('Nate', '564', 'nate@nate.com', 'UCLA');
+    
+    expect(intern.getName()).toBe('Nate');
+    expect(intern.getId()).toBe('564');
+    expect(intern.getEmail()).toBe('nate@nate.com');
+
+    expect(intern.getRole()).toBe('Intern');
+
+    expect(intern.getSchool()).toBe('UCLA');
+});
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,24 @@
 const { TestScheduler } = require('jest');
+const Manager = require('../lib/Manager');
+
+// test for Manager name, id, and email
+test ('checks to see if Manager name is string', ()=> {
+    const manager = new Manager('Nate', '564', 'nate@nate.com', '6');
+    
+    expect(manager.name).toBe('Nate');
+    expect(manager.id).toBe('564');
+    expect(manager.email).toBe('nate@nate.com');
+});
+
+// test for Manager getname, getid,  getemail, and getgithub methods 
+test ('checks to see if Manager name is string', ()=> {
+    const manager = new Manager('Nate', '564', 'nate@nate.com', '6');
+    
+    expect(manager.getName()).toBe('Nate');
+    expect(manager.getId()).toBe('564');
+    expect(manager.getEmail()).toBe('nate@nate.com');
+
+    expect(manager.getRole()).toBe('Manager');
+
+    expect(manager.getOfficeNumber()).toBe('6');
+});
